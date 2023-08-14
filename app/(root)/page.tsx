cd9e286bd4b9b8176997aa8471abf8a3b235e857
bruceliu <bruceliu1107@gmail.com>
2023-08-14 10:51:40 +0800

    feat: use-store-modal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
-'use client'
+"use client";
+
+import { Modal } from "@/components/ui/modal";
 
 const SetupPage = () => {
   return (
     <div className="p-4">
+      <Modal title="Test" description="Test Description" isOpen onClose={() => {}}>
+        Children
+      </Modal>
     </div>
   );
 };
