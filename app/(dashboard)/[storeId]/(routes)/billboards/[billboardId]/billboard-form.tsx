9adb390e1bcf312ee25bf2dee7c4c40452f6575c
bruceliu <bruceliu1107@gmail.com>
2023-09-01 09:41:55 +0800

    billboard quick action delete and edit

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/billboard-form.tsx
@@ -82,12 +82,13 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
       setLoading(true);
       await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
-      router.push("/"); // we can direct the user to other existing store
+      router.push(`/${params.storeId}/billboards`)
       toast.success("Billboard deleted");
     } catch (error) {
       // safety mechanism
       // our relation in prisma is going to control this, to ensure the safety
       toast.error("Make sure you removed all categories using this billboard first");
+    }finally{
       setLoading(false);
       setOpen(false);
     }
